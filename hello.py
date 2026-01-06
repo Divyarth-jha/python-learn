cce0d35b2dbf2631ce094229785acf12d6c296dc
Divyarth-jha <divyarthjha@gmail.com>
2026-01-06 13:07:32 +0530

    solve some question

--- hello.py
+++ hello.py
@@ -1,1 +1,14 @@
 print("hello world ! ")
+
+# Q.1- print the sum of two number by taking input
+
+# num1 = int(input("enter first number:"))
+# num2 = int(input("enter second number:"))
+
+# print(num1 + num2)
+
+#Q.2 enter the side of squre and give and area
+
+side = int(input("Enter the side of square: "))
+
+print("Area of Square is:",side ** 2)
