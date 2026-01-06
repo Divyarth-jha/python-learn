bec9e3713c0feb9c1c3336b95e0f38dacf752d3f
Divyarth-jha <divyarthjha@gmail.com>
2026-01-06 23:35:54 +0530

    add string file

---
+++ string.py
@@ -0,0 +1,16 @@
+# str1 ='this is string.'
+# str2 =" learning"
+# str3 = """this is a string"""
+
+
+# str4 = "This is a string /n we show" 
+
+# print(len(str4))
+#index
+#slicing
+
+
+#str = "ApnaCollege"
+#str [ 1: 4] = its give   "pna"  from index 1 to 4
+# str [ : 4] = its give same as str[0:4]
+#str [1: ] = it give  same str[ 1: len(str)]
