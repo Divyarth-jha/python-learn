0dbca1b2f8458f877e96d9cbf805135e7e0da36c
Divyarth-jha <divyarthjha@gmail.com>
2026-01-07 01:12:47 +0530

    sole some question

---
+++ condition.py
@@ -0,0 +1,4 @@
+# # conditional statements  - it return true or false 
+# if 
+# elif
+# else
--- string.py
+++ string.py
@@ -14,3 +14,20 @@
 #str [ 1: 4] = its give   "pna"  from index 1 to 4
 # str [ : 4] = its give same as str[0:4]
 #str [1: ] = it give  same str[ 1: len(str)]
+
+# #str = "i am sdtudying python from ApnaCollege"
+# str = "i am studing js from apna college"
+# # print(str.replace("js","python"))
+# print(str.find("o"))
+
+#Q.1 Take input frome user and print there length
+
+# name = input("enter your name: ")
+
+# print(len(name)) ans 6
+
+#write a programe find the $ accourense in the string
+
+# str = "karan give me 100$ and i spend 50$ in gas and 15$ in toll and 35$ in hotel"
+
+# print (str.count("$")) ans 5
