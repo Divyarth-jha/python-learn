5e2b98bd63fdcc42470503325069e356b1ae9571
Divyarth-jha <divyarthjha@gmail.com>
2026-01-12 03:58:18 +0530

    list and tuple question and basic

--- list.py
+++ list.py
@@ -1,3 +1,32 @@
-marks =[ 12, 34, 45, 56, 67, 98, 96]
+# marks =[ 12, 34, 45, 56, 67, 98, 96]
+# marks.append(67)
+# marks.sort( reverse =True)
+# print((marks))
 
-print(type(marks))
+# list =[ 2,1,3]
+# print(list.append(4)) # this append method is not creatinh any new  list   same for short
+# print(list.sort())
+# print(list)
+
+
+# write the  program  to check if a list contain  a pailbdrome of elements (Hint: use copy() method)
+
+# list = [1,2,3,2,1]
+
+# list2 = list.copy()
+# list2.reverse()
+
+# if list == list2 :
+#     print("it is palamdro")
+# else:
+#     print("not a palamdro")    
+
+
+grade=("A", "A", "D", "D", "A" ,"B" ,"B", "C","D" ,"C", "B")
+print(grade.count("A"))
+print(type(grade))
+
+
+grade1=["A", "A", "D", "D", "A" ,"B" ,"B", "C","D" ,"C", "B"]
+grade1.sort()
+print(grade1)
