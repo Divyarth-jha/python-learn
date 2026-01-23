a1da38321bac90e9f93c157da82ad4161882581e
Divyarth-jha <divyarthjha@gmail.com>
2026-01-24 03:30:20 +0530

    last try  for friday

--- n8n.py
+++ n8n.py
@@ -1,3 +1,12 @@
-get =(9,8,7,8,9,67)
+nums = [1, 2, 2, 3, 1, 4, 3]
 
-print(ge)
+unique_nums = []
+seen = set()
+
+for x in nums:
+    if x not in seen:
+        unique_nums.append(x)
+        seen.add(x)
+
+print("Original:", nums)
+print("Without duplicates:" unique_nums)
